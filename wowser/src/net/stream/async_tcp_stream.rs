f9a43bc3c4279470eec3a951ae101ccf8dc5b298
Dustin Toff <me@dustintoff.com>
2020-05-31 00:23:25 +0100

    Removed commented out code

--- wowser/src/net/stream/async_tcp_stream.rs
+++ wowser/src/net/stream/async_tcp_stream.rs
@@ -9,12 +9,6 @@ impl AsyncTcpStream {
     pub fn from_tcp_stream(stream: net::TcpStream) -> AsyncTcpStream {
         AsyncTcpStream { stream }
     }
-
-    // pub fn read_chunk<'a>(&'a mut self) -> AsyncTcpStreamChunk<'a> {
-    //     AsyncTcpStreamChunk {
-    //         stream: self.stream,
-    //     }
-    // }
 }
 
 impl futures::stream::Stream for AsyncTcpStream {
@@ -38,27 +32,3 @@ impl futures::stream::Stream for AsyncTcpStream {
         }
     }
 }
-
-// struct AsyncTcpStreamChunk<'a> {
-//     stream: net::TcpStream
-// }
-
-// impl<'a> futures::stream::Stream for AsyncTcpStreamChunk<'a> {
-//     type Output = Result<Vec<u8>, std::io::Error>;
-
-//     fn poll(self: std::pin::Pin<&mut Self>, cx: &mut task::Context<'_>) -> task::Poll<Self::Output> {
-//         let read_amt = self.stream.peek(&mut [0u8; 1]);
-//         match read_amt {
-//             Ok(0) => task::Poll::Pending,
-//             Ok(_) => {
-//                 let mut ret = [0u8; 512];
-//                 match self.get_mut().stream.read(&mut ret) {
-//                     Ok(_) => task::Poll::Ready(Ok(ret.to_vec())),
-//                     Err(e) => task::Poll::Ready(Err(e))
-//                 }
-//             },
-//             Err(e) => task::Poll::Ready(Err(e))
-//         }
-//     }
-
-// }

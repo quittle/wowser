ac39372ea21183238a0f7e8764f14668566fac51
Dustin Toff <me@dustintoff.com>
2020-10-11 14:48:58 +0100

    Upgrade Rust toolchain to 1.47.0
    - Disabled clippy check on wowser-gl-sys that caused clippy failures in CI

--- wowser-gl-sys/src/gl.rs
+++ wowser-gl-sys/src/gl.rs
@@ -3,5 +3,6 @@
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
+#![allow(clippy::redundant_static_lifetimes)]
 #![allow(clippy::unreadable_literal)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));

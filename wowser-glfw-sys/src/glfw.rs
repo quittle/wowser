ee59bdf90cdf8d9d049a9de556aa999bf30b6f41
Dustin Toff <me@dustintoff.com>
2021-06-07 23:00:57 -0400

    Fix rustdoc warnings

--- wowser-glfw-sys/src/glfw.rs
+++ wowser-glfw-sys/src/glfw.rs
@@ -5,4 +5,5 @@
 #![allow(non_upper_case_globals)]
 #![allow(clippy::unreadable_literal)]
 #![allow(clippy::from_over_into)]
+#![allow(rustdoc::broken_intra_doc_links)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));

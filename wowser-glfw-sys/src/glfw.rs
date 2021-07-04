efcf2751cd2f76d9aa5199110e7c452378cf471c
Dustin Toff <me@dustintoff.com>
2021-07-04 13:43:28 -0400

    Upgrade deps and Rust version
    Also update dependency specifications to '*'

--- wowser-glfw-sys/src/glfw.rs
+++ wowser-glfw-sys/src/glfw.rs
@@ -1,5 +1,6 @@
 //! Contains all rust declarations of generated bindings.
 
+#![allow(deref_nullptr)] // See https://github.com/rust-lang/rust-bindgen/issues/1651
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
